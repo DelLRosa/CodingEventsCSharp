45a3134588b7c4ec339875f502f07cfa90c3ef14
Delaney Green <delaney.rosa1@gmail.com>
2024-06-03 10:26:52 -0500

    Setup coding events with static Events list + basic form

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,30 @@
+﻿namespace Coding_Events;
+using Microsoft.AspNetCore.Mvc;
+public class EventsController : Controller
+{
+    static private List<string> Events = new List<string>{"Gundam Build Night", "Salsa Class", "Bike Ride"};
+
+    // GET: <port>/events
+    [HttpGet]
+    public IActionResult Index() 
+    {
+        
+        ViewBag.events = Events;
+        return View();
+
+    }
+    // Get: <port>/events/add
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/Events/Add")]
+    public IActionResult NewEvent(string name)
+    {
+        Events.Add(name);
+        return Redirect("/Events");
+    }
+}
