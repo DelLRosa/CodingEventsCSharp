7d9a1234ef85d8456d0043808681ca406f84ec8c
Delaney Green <delaney.rosa1@gmail.com>
2024-06-11 10:21:22 -0500

    ORM studio 1 complete

--- Controllers/EventCategoryController.cs
+++ Controllers/EventCategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Coding_Events.Models;
 using Coding_Events.Data;
+using Coding_Events.ViewModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Coding_Events;
 
@@ -19,5 +21,29 @@ public class EventCategoryController : Controller
     {
         List<EventCategory> categories = context.Categories.ToList();
         return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        AddEventCategoryViewModel addEventCategoryViewModel= new();
+        return View(addEventCategoryViewModel);
+    }
+
+    [HttpPost("/createCategory")]
+    public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+    {
+        if(ModelState.IsValid)
+        {
+            EventCategory newEventCategory = new EventCategory
+            {
+                Name = addEventCategoryViewModel.Name
+            };
+            context.Categories.Add(newEventCategory);
+            context.SaveChanges();
+
+            return Redirect("/eventcategory/Index");
+        }
+        return View("Create",addEventCategoryViewModel);
     }
 }
---
+++ ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coding_Events.ViewModels;
+
+public class AddEventCategoryViewModel
+{
+    [Required(ErrorMessage ="This field is required")]
+    [StringLength(20, MinimumLength =3,ErrorMessage ="Name must be between 3 and 20 characters long")]
+    public string? Name{get;set;}
+}
